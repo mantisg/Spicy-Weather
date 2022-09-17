3947505866e526afc22276d020df94bf9a2ffee5
GWNR71517\joshu <joshuabliven93@gmail.com>
2022-09-17 13:38:04 -0400

    Original initiation of basic layout

--- App.js
+++ App.js
@@ -3,18 +3,48 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.page}>
+      <View style={styles.header}>
+        <Header />
+      </View>
+      <View style={styles.container}>
+        <Text>Lets get spicy!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+const Header = () => {
+  return (
+    <View>
+      <Text>
+        Spicy Forecast
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: '#b40240',
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b40240',
     alignItems: 'center',
     justifyContent: 'center',
+    fontWeight: 'bold',
   },
+
+  header: {
+    backgroundColor: '#eeeeee',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+  },
+
 });
